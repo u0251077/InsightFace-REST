40a35c2057359e78b018b0c2aef339867439a6c4
Pei Ying <u0251077@gmail.com>
2021-11-29 14:57:59 +0800

    add the webcam example in client.py

--- client.py
+++ client.py
@@ -126,3 +126,70 @@ print("first face bbox:")
 print(images[0]['faces'][0]['bbox'])#left-up x,y, right-down x,y
 print("first face 512dims feature:")
 print(images[0]['faces'][0]['vec'])#left-up x,y, right-down x,y
+
+# webcam demo
+import cv2
+from numpy import dot
+from numpy.linalg import norm
+
+import listname as SSS
+
+# 選擇第二隻攝影機
+cap = cv2.VideoCapture(0)
+import numpy as np
+while(True):
+  # 從攝影機擷取一張影像
+  ret, frame = cap.read()
+
+  jpg_as_text = base64.b64encode(cv2.imencode('.jpg', frame)[1]).decode()
+  params={
+   "images": {
+     "data": [
+       jpg_as_text
+     ]
+   },
+   "threshold": 0.6,
+   "embed_only": False,
+   "return_face_data": False,
+   "return_landmarks": False,
+   "extract_embedding": True,
+   "extract_ga": False,
+   "limit_faces": 0,
+   "min_face_size": 0,
+   "verbose_timings": True,
+   "api_ver": "2",
+   "msgpack": False
+  }
+  result = requests.post(url,json.dumps(params))
+  content = ujson.loads(result.text)
+  
+  took = content.get('took')
+  status = content.get('status')
+  images = content.get('data')
+  # face number
+  #print(len(images[0]['faces']))
+  for i in range(len(images[0]['faces'])):
+
+      # draw face bbox
+      cv2.rectangle(frame, (images[0]['faces'][i]['bbox'][0], images[0]['faces'][i]['bbox'][1]), (images[0]['faces'][i]['bbox'][2], images[0]['faces'][i]['bbox'][3]), (0, 0, 255), 2, cv2.LINE_AA)  
+      # write the 512 feature in picture
+      cv2.putText(frame, str(images[0]['faces'][i]['vec']), (images[0]['faces'][i]['bbox'][0], images[0]['faces'][i]['bbox'][1]), cv2.FONT_HERSHEY_SIMPLEX,  1, (0, 255, 0), 1, cv2.LINE_AA)
+      # example for match face
+      #cos_sim = dot(feature1, images[0]['faces'][i]['vec'])/(norm(feature1)*norm(images[0]['faces'][i]['vec']))
+      #if cos_sim > 0.6:
+      #    print("it the same")
+      
+
+              
+              
+  cv2.imshow('frame', frame)
+  
+  # 若按下 q 鍵則離開迴圈
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+
+# 釋放攝影機
+cap.release()
+
+# 關閉所有 OpenCV 視窗
+cv2.destroyAllWindows()
